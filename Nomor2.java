e8dd8cc2bf181f538c1f7005bdabef9a7dd6917b
Parrr25 <126962860+Parrr25@users.noreply.github.com>
2024-12-12 08:43:26 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,51 +1,52 @@
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Nomor2 {
-    public static final int[] hargaMenu = {35000, 45000, 20000, 15000};
-    public static ArrayList<Integer> keranjangBelanja = new ArrayList<>();
+    private static final int[] hargaMenu = {35000, 45000, 20000, 15000};
+    private static final String[] namaMenu = {"Burger", "Steak", "Spaghetti", "French fries"};
+    private static ArrayList<Integer> keranjangBelanja = new ArrayList<>();
 
     public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
-            String pilihan;
-            
-            do {
-                System.out.print("Masukkan nama menu [Burger/Steak/Spaghetti/French fries]: ");
-                String menu = input.nextLine();
+        Scanner input = new Scanner(System.in);
+        String pilihan;
 
-                // Menentukan harga berdasarkan input pengguna
-                int harga = 0;
-                switch (menu.toLowerCase()) {
-                    case "burger":
-                        harga = hargaMenu[0];
-                        break;
-                    case "steak":
-                        harga = hargaMenu[1];
-                        break;
-                    case "spaghetti":
-                        harga = hargaMenu[2];
-                        break;
-                    case "french fries":
-                        harga = hargaMenu[3];
-                        break;
-                    default:
-                        System.out.println("Menu tidak tersedia.");
-                        continue; 
-                }
+        do {
+            System.out.print("Masukkan nama menu: ");
+            String menu = input.nextLine();
 
-                keranjangBelanja.add(harga);
-                
-                System.out.print("Input lagi (Y/N)?: ");
-                pilihan = input.nextLine();
-            } while (pilihan.equalsIgnoreCase("Y"));
-        }
+            int harga = 0;
+            switch (menu.toLowerCase()) {
+                case "burger":
+                    harga = hargaMenu[0];
+                    break;
+                case "steak":
+                    harga = hargaMenu[1];
+                    break;
+                case "spaghetti":
+                    harga = hargaMenu[2];
+                    break;
+                case "french fries":
+                    harga = hargaMenu[3];
+                    break;
+                default:
+                    System.out.println("Menu tidak tersedia.");
+                    continue; 
+            }
+
+            keranjangBelanja.add(harga);
+
+            System.out.print("Input lagi (Y/N)?: ");
+            pilihan = input.nextLine();
+        } while (pilihan.equalsIgnoreCase("Y"));
+
         int totalBelanja = hitungTotalBelanja();
         double diskon = hitungDiskon(totalBelanja);
         double totalBayar = totalBelanja - diskon;
 
         System.out.println("Total item pesanan = " + keranjangBelanja.size());
         System.out.println("Total yang harus dibayar = " + totalBayar);
+
+        input.close();
     }
 
     public static int hitungTotalBelanja() {
