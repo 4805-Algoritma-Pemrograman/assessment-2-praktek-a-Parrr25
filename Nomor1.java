02b324182dad085739f9f0993fb080a3a499eaff
Parrr25 <126962860+Parrr25@users.noreply.github.com>
2024-12-12 08:05:05 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,14 +2,24 @@ import java.util.Scanner;
 
 public class Nomor1 {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan bilangan n: ");
         int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-
-        /* Pekerjaan anda berakhir sini */
+        
+        // Memanggil prosedur untuk menampilkan angka ganjil
+        tampilkanAngkaGanjil(N);
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
-    
+
+    // Prosedur untuk menampilkan angka ganjil dari 0 hingga n
+    public static void tampilkanAngkaGanjil(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) { 
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(); 
+    }
 }
