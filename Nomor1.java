66c58f00f5fb934f41936d1268567e0547820493
Parrr25 <126962860+Parrr25@users.noreply.github.com>
2024-12-12 08:25:16 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class Nomor1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Masukkan bilangan n: ");
-        int n = scanner.nextInt();
-        tampilkanGanjil(n);
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Masukkan bilangan n: ");
+            int n = scanner.nextInt();
+            tampilkanGanjil(n);
+        }
     }
 
     public static void tampilkanGanjil(int n) {
         for (int i = 0; i <= n; i++) {
             if (i % 2 != 0) {
-                System.out.println(i);
+                System.out.print(i);
             }
         }
     }
